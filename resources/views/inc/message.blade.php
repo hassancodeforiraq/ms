d1f6d1db6f54930d633505d7c779b2e28d757458
hassancodeforiraq <49936474+hassancodeforiraq@users.noreply.github.com>
2019-06-23 04:06:57 +0300

    fiir

---
+++ resources/views/inc/message.blade.php
@@ -0,0 +1,3 @@
+@if (Session('success'))
+    <div class="alert alert-success">{{ session('success') }}</div>
+@endif
---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron text-center">
+    <div class="container">
+        <h1>Welcome to Laravel 5.8</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi minus corporis consequatur commodi odit alias quasi ducimus nostrum autem laborum?</p>
+    </div>
+</div>
